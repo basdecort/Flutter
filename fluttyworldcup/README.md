a5c2079956e3a0bdbba52e78e7cee29060f73165
Bas de Cort <bas.decort@infosupport.com>
2018-05-31 12:50:10 +0200

    Created core simulation

---
+++ fluttyworldcup/README.md
@@ -0,0 +1,8 @@
+# fluttyworldcup
+
+Flutty World Cup
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ fluttyworldcup/lib/core.dart
@@ -0,0 +1,112 @@
+import 'dart:async';
+
+import 'dart:math';
+class Team
+{
+  Team({this.name, this.strength});
+
+  final String name;
+  final int strength;
+}
+
+class GameEventRecord
+{
+  GameEventRecord({this.event, this.state, this.minute, this.target});
+
+  final GameState state;
+  final GameEvent event;
+  final int minute;
+  final Team target;
+}
+
+enum GameEvent
+{
+  GoalScored,
+  NearMiss,
+  Tackle,
+  Penalty,
+  RedCard,
+  YellowCard,
+  Corner,
+  FreeKick,
+  Substitute,
+  TimerTick
+}
+
+enum GameState
+{
+  Starting,
+  Started,
+  Paused,
+  Ended
+}
+
+class Game
+{
+  Stream<GameEventRecord> get gameEvent { return gameEventController.stream; }
+
+  int minute = 0;
+  StreamController gameEventController = new StreamController.broadcast();
+
+  Team a;
+  Team b;
+
+  Game.Start({this.a, this.b})
+  {
+    minute = 0;
+
+    new Timer.periodic(new Duration(milliseconds: 200), (Timer t){
+
+        if (minute == 106)
+        {
+          t.cancel();
+           gameEventController.close();
+        }else{
+          calcEvent(minute++);
+        }
+
+    });
+  }
+
+  calcEvent(int minute)
+  {
+     var gameState =  minute == 0 ? GameState.Starting : minute == 105 ? GameState.Ended : (minute > 45 && minute < 60) ? GameState.Paused : GameState.Started;
+
+     gameEventController.add(new GameEventRecord(event: GameEvent.TimerTick, state: gameState,minute: minute, target: null));
+     var chance = new Random().nextInt(100) % (minute < 15 || minute > 75 ? 5 : 10) == 0;
+
+     if (chance && gameState == GameState.Started)
+     {
+       int rnd = new Random().nextInt(GameEvent.values.length-1);
+       var team = new Random().nextInt(2) == 0 ? a : b;
+       var event = GameEvent.values[rnd];
+
+       if (event == GameEvent.GoalScored)
+       {
+          int diff = (a.strength - b.strength).abs();
+          bool goal = diff > 10 ?  new Random().nextInt(5) == 0 : diff > 5 ? new Random().nextInt(3) == 0 : new Random().nextInt(2) == 0;
+          if (goal)
+          {
+            gameEventController.add(new GameEventRecord(event: event, state: gameState, minute: minute, target: team));
+          }else
+          {
+            gameEventController.add(new GameEventRecord(event: GameEvent.NearMiss, state: gameState, minute: minute, target: team));
+          }
+       }
+       else
+       {
+         gameEventController.add(new GameEventRecord(event: event, state: gameState, minute: minute, target: team));
+       }
+     }
+  }
+
+}
+
+main()
+{
+  var game = new Game.Start(a:new Team(name: "Nederland", strength: 100), b:new Team(name: "Duitsland", strength: 10));
+  game.gameEvent.listen((GameEventRecord e){
+    print("${e.minute}: ${e.state}  ${e.event} ${e.target?.name}");
+  });
+}
+
